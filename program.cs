05c98581b32c91177af415b7c6b9da2b364a164e
studentidgithub43 <111099541+studentidgithub43@users.noreply.github.com>
2022-08-11 18:49:33 -0700

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class HelloWorld
+{
+     class CFilter{
+            int size = 31;
+            
+            static void Main(string[] args){
+                CFilter CF_001 = new CFilter();
+                Console.WriteLine("The size of the coffee filter #1 is : "+CF_001.size);
+            }
+        }
+        
+    
+}
